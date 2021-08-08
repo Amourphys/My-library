e13dc886477cf56c7b7ac6cd5da866369f6b3a38
Jurij <amourphys@gmail.com>
2021-08-08 15:06:16 +0300

    changed index.html, main.js, actions.js, lib.js and create effect.js & _variables.scss

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,20 @@
 import $ from './lib/lib';
 
 $('button').on('click', function() {
-    $(this).toggleClass('active');
+    $('div').eq(2).toggleClass('active');
 });
+
+$('div').click(function() {
+    console.log($(this).index());
+});
+
+console.log($('div').eq(2).find('.more'));
+console.log($('.some').closest('.findme'));
+console.log($('.more').eq(0).siblings());
+$('button').fadeOut(1800);
+$('button').fadeIn(1800);
+
+//console.log($('button').html());
 
 /* function sayHello() {
     console.log('Hello!');
